1fe6bac92328ad8e61467de10be2081114148c91
NewAnton <1anton1korobov@gmail.com>
2022-05-30 20:33:57 +0300

    refactor: rename classes by using BEM according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -7,9 +7,9 @@
 
 
 // Event handling, user interaction is what starts the code execution.
-
-var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+ 
+var taskInput=document.getElementsByClassName("add-item__input")[0];//Add a new task.
+var addButton=document.getElementsByClassName("add-item__button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -43,7 +43,7 @@ var createNewTaskElement=function(taskString){
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="main__delete-button";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -157,7 +157,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
     var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var deleteButton=taskListItem.querySelector(".main__delete-button");
 
 
     //Bind editTask to edit button.
